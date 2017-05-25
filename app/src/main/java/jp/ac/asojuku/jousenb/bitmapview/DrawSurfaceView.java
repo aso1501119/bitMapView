d5f4e9a3ee7dee2b45419a904c8711e20f835814
Takatsu Shutaro <1501119@st.asojuku.ac.jp>
2017-05-25 14:48:34 +0900

    Initial commit

---
+++ app/src/main/java/jp/ac/asojuku/jousenb/bitmapview/DrawSurfaceView.java
@@ -0,0 +1,203 @@
+package jp.ac.asojuku.jousenb.bitmapview;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.PixelFormat;
+import android.graphics.PorterDuff;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+/**
+ * Created by Syu on 2017/05/25.
+ */
+
+public class DrawSurfaceView extends SurfaceView implements SurfaceHolder.Callback {
+
+    private SurfaceHolder mHolder;
+    private Paint   mPaint;
+    private Path    mPath;
+    private Bitmap mLastDrawBitmap;
+    private Canvas mLastDrawCanvas;
+    private Deque<Path> mUndoStack = new ArrayDeque<Path>();
+    private Deque<Path> mRedoStack = new ArrayDeque<Path>();
+
+    public DrawSurfaceView(Context context){
+        super(context);
+        init();
+    }
+
+    public DrawSurfaceView(Context context, AttributeSet attrs){
+        super(context,attrs);
+        init();
+    }
+
+    private void init(){
+        mHolder = getHolder();
+
+        //透過
+        setZOrderOnTop(true);
+        mHolder.setFormat(PixelFormat.TRANSPARENT);
+
+        //コールバックメソッドを設定
+        mHolder.addCallback(this);
+
+        //ペイントの設定
+        mPaint = new Paint();
+        mPaint.setColor(Color.BLUE);
+        mPaint.setStyle(Paint.Style.STROKE);
+        mPaint.setStrokeCap(Paint.Cap.ROUND);
+        mPaint.setAntiAlias(true);
+        mPaint.setStrokeWidth(6);
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        //描画状態を保存するBitmapを作成する
+        clearLastDrawBitmap();
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        mLastDrawBitmap.recycle();
+    }
+
+    private void clearLastDrawBitmap(){
+        if(mLastDrawBitmap == null){
+            mLastDrawBitmap = Bitmap.createBitmap(getWidth(),getHeight(), Bitmap.Config.ARGB_8888);
+        }
+        if(mLastDrawCanvas == null){
+            mLastDrawCanvas = new Canvas(mLastDrawBitmap);
+        }
+
+        mLastDrawCanvas.drawColor(0, PorterDuff.Mode.CLEAR);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        switch (event.getAction()){
+            case MotionEvent.ACTION_DOWN:   //画面にタッチした時
+                onTouchDown(event.getX(),event.getY());
+                break;
+
+            case MotionEvent.ACTION_MOVE:   //指を動かした時
+                onTouchMove(event.getX(),event.getY());
+                break;
+
+            case MotionEvent.ACTION_UP:  //画面からゆびを離したとき
+                onTouchUp(event.getX(),event.getY());
+                break;
+
+            default:
+        }
+        return true;
+    }
+
+    private void onTouchDown(float x, float y){
+        mPath = new Path();
+        mPath.moveTo(x, y);
+    }
+
+    private void onTouchMove(float x, float y){
+        mPath.lineTo(x,y);
+        drawLine(mPath);
+    }
+
+    private void onTouchUp(float x ,float y){
+        mPath.lineTo(x, y);
+        drawLine(mPath);
+        mLastDrawCanvas.drawPath(mPath,mPaint);
+        mUndoStack.addLast(mPath);
+        mRedoStack.clear();
+    }
+
+    private void drawLine(Path path){
+        //ロックしてキャンバス取得
+        Canvas canvas = mHolder.lockCanvas();
+
+        //キャンバスをクリア
+        canvas.drawColor(0, PorterDuff.Mode.CLEAR);
+
+        //前回描画したビットマップをキャンバスに描画
+        canvas.drawBitmap(mLastDrawBitmap,0,0,null);
+
+        //パスを描画する
+        canvas.drawPath(path,mPaint);
+
+        //ロックを外す
+        mHolder.unlockCanvasAndPost(canvas);
+
+    }
+
+    public void undo(){
+        if(mUndoStack.isEmpty()){
+            return;
+        }
+
+        //udoスタックからパスを取り出しredoスタックに格納
+        Path lastUndoPath = mUndoStack.removeLast();
+        mRedoStack.addLast(lastUndoPath);
+
+        //ロックしてキャンバスを取得する
+        Canvas canvas = mHolder.lockCanvas();
+
+        //キャンバスをクリアします
+        canvas.drawColor(0, PorterDuff.Mode.CLEAR);
+
+        //描画状態を保持するBitmapをクリア
+        clearLastDrawBitmap();
+
+        //パスを描画する
+        for (Path path : mUndoStack){
+            canvas.drawPath(path,mPaint);
+            mLastDrawCanvas.drawPath(path,mPaint);
+        }
+
+        //ロックを外す
+        mHolder.unlockCanvasAndPost(canvas);
+    }
+
+    public void  redo(){
+        if(mRedoStack.isEmpty()){
+            return;
+        }
+
+        // redoスタックからパスを取り出し、undoスタックに格納します
+        Path lastRedoPath = mRedoStack.removeLast();
+        mUndoStack.addLast(lastRedoPath);
+
+        //パスを描画します
+        drawLine(lastRedoPath);
+
+        mLastDrawCanvas.drawPath(lastRedoPath,mPaint);
+    }
+
+    public void reset(){
+
+        //スタックをからにする
+        mUndoStack.clear();
+        mRedoStack.clear();
+
+
+        clearLastDrawBitmap();
+
+        Canvas canvas = mHolder.lockCanvas();
+        canvas.drawColor(0, PorterDuff.Mode.CLEAR);
+        mHolder.unlockCanvasAndPost(canvas);
+    }
+
+
+}
---
+++ app/src/main/java/jp/ac/asojuku/jousenb/bitmapview/MainActivity.java
@@ -0,0 +1,52 @@
+package jp.ac.asojuku.jousenb.bitmapview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private DrawSurfaceView mCamvasView;
+    private Button undo;
+    private Button redo;
+    private Button reset;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        mCamvasView = (DrawSurfaceView)findViewById(R.id.canvasView);
+
+        undo = (Button)findViewById(R.id.undoBtn);
+
+        undo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCamvasView.undo();
+            }
+        });
+
+        redo = (Button)findViewById(R.id.redoBtn);
+        redo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCamvasView.redo();
+            }
+        });
+
+        reset = (Button)findViewById(R.id.resetBtn);
+
+        reset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCamvasView.reset();
+            }
+        });
+
+
+    }
+}
