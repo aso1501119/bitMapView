fca2a4dd71db7e1deb42602551584afb0ec5f4b0
aso1501139 <murofushi@172.20.81.119>
2017-06-06 12:29:54 +0900

    test

--- app/src/main/java/jp/ac/asojuku/jousenb/bitmapview/MainActivity.java
+++ app/src/main/java/jp/ac/asojuku/jousenb/bitmapview/MainActivity.java
@@ -1,56 +1,65 @@
 package jp.ac.asojuku.jousenb.bitmapview;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
+import jp.ac.asojuku.jousenb.bitmapview.entity.PeerInfo;
+import jp.ac.asojuku.jousenb.bitmapview.view.DrawSurfaceView;
+
+/**
+ * Created by Syu on 2017/05/25.
+ * Edited by Murofushi on 2017/06/06.
+ */
+
 public class MainActivity extends AppCompatActivity {
 
-
+    private PeerInfo mInfo;
+    private Context mContext;
     private DrawSurfaceView mCamvasView;
-    private Button undo;
-    private Button redo;
-    private Button reset;
-    private Button color;
+    private Button mUndoButton;
+    private Button mRedoButton;
+    private Button mResetButton;
+    private Button mColorButton;
+    private Button mConnectButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        mContext = this.getApplicationContext();
         mCamvasView = (DrawSurfaceView)findViewById(R.id.canvasView);
 
-        undo = (Button)findViewById(R.id.undoBtn);
+        mUndoButton = (Button)findViewById(R.id.undoBtn);
 
-        undo.setOnClickListener(new View.OnClickListener() {
+        mUndoButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 mCamvasView.undo();
             }
         });
 
-        redo = (Button)findViewById(R.id.redoBtn);
-        redo.setOnClickListener(new View.OnClickListener() {
+        mRedoButton = (Button)findViewById(R.id.redoBtn);
+        mRedoButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 mCamvasView.redo();
             }
         });
 
-        reset = (Button)findViewById(R.id.resetBtn);
-
-        reset.setOnClickListener(new View.OnClickListener() {
+        mResetButton = (Button)findViewById(R.id.resetBtn);
+        mResetButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
                 mCamvasView.hutosa();
             }
         });
 
-        color = (Button)findViewById(R.id.colorBtn);
-        color.setOnClickListener(new View.OnClickListener() {
+        mColorButton = (Button)findViewById(R.id.colorBtn);
+        mColorButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 mCamvasView.colorChange();
@@ -58,6 +67,25 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        mConnectButton = (Button)findViewById(R.id.connectBtn);
+        mConnectButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                v.setEnabled(false);
+                if (!mInfo.isConnectFlag()) {
+                    // Calling dialog
+                    mInfo.listingPeers();
+                } else {
+                    // Close
+                    mInfo.closing();
+                }
+
+                v.setEnabled(true);
+            }
+        });
+
+        //初期値宣言
+        mInfo = new PeerInfo(mContext);
 
     }
 }
